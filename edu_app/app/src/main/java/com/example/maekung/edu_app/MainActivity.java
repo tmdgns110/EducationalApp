3f0e91d0e6b5747d54736c00b0d17fe96ebce5a9
younsoo Lim <maekung3@gmail.com>
2017-12-01 02:17:03 -0800

    init screen for android

--- edu_app/app/src/main/java/com/example/maekung/edu_app/MainActivity.java
+++ edu_app/app/src/main/java/com/example/maekung/edu_app/MainActivity.java
@@ -1,87 +1,38 @@
 package com.example.maekung.edu_app;
 
-import android.os.Handler;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import butterknife.ButterKnife;
-import butterknife.BindView;
-import is.arontibo.library.ElasticDownloadView;
-import is.arontibo.library.ProgressDownloadView;
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.elastic_download_view)
-    ElasticDownloadView mElasticDownloadView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        ImageView plus = (ImageView)findViewById(R.id.plus);
+        ImageView minus = (ImageView)findViewById(R.id.minus);
+        ImageView multiply = (ImageView)findViewById(R.id.multiply);
+        ImageView divide = (ImageView)findViewById(R.id.divide);
+
+        plus.setImageResource(R.drawable.plus);
+        minus.setImageResource(R.drawable.minus);
+       multiply.setImageResource(R.drawable.multiply);
+        divide.setImageResource(R.drawable.divide);
+
+
+        TextView myText = (TextView) findViewById(R.id.intro);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(50); //You can manage the time of the blink with this parameter
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        myText.startAnimation(anim);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_run_success_animation) {
-
-            new Handler().post(new Runnable() {
-                @Override
-                public void run() {
-                    mElasticDownloadView.startIntro();
-                }
-            });
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mElasticDownloadView.success();
-                }
-            }, 2 * ProgressDownloadView.ANIMATION_DURATION_BASE);
-
-            return true;
-        } else if (id == R.id.action_run_fail_animation) {
-
-            new Handler().post(new Runnable() {
-                @Override
-                public void run() {
-                    mElasticDownloadView.startIntro();
-                }
-            });
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mElasticDownloadView.setProgress(45);
-                }
-            }, 2 * ProgressDownloadView.ANIMATION_DURATION_BASE);
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mElasticDownloadView.fail();
-                }
-            }, 3 * ProgressDownloadView.ANIMATION_DURATION_BASE);
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
